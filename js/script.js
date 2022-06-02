bb4dce5621b441cbbe9e0ebde6c55499b4b979c9
Volodymyr Kalashnikov <imaddybb@gmail.com>
2022-06-03 02:20:58 +0300

    Rhythm

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict"
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    autoHeight:true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+  });
